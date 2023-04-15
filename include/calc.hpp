f13e097bc32e822ef64c64dfd6fb93fa5cbd6533
Daniel Krawisz <danielkrawisz@protonmail.com>
2023-04-15 10:34:16 -0500

    Fix up calculator a bit

--- include/calc.hpp
+++ include/calc.hpp
@@ -14,7 +14,7 @@ namespace calc {
 
     struct hex_digit : seq<xdigit, xdigit> {};
     struct hex_lit : seq<string<'0', 'x'>, star<hex_digit>> {};
-    struct dec_lit : seq<range<'1', '9'>, star<digit>> {};
+    struct dec_lit : sor<one<'0'>, seq<range<'1', '9'>, star<digit>>> {};
 
     struct number_lit : sor<dec_lit, hex_lit> {};
 
@@ -30,17 +30,17 @@ namespace calc {
 
     struct open_paren : one<'('> {};
     struct close_paren : one<')'> {};
-    struct whitespace : star<space> {};
+    struct ws : star<space> {};
 
     struct expression;
 
-    struct factor : sor<number_lit, string_lit, symbol, seq<open_paren, whitespace, expression, whitespace, close_paren>> {};
+    struct factor : sor<number_lit, string_lit, symbol, seq<open_paren, ws, expression, ws, close_paren>> {};
 
     struct unary_operator : sor<one<'~'>, one<'!'>, one<'+'>, one<'-'>, one<'*'>> {};
 
     struct comp_operator : sor<string<'<','='>, string<'>','='>, string<'=','='>, string<'!','='>, one<'>'>, one<'<'>> {};
 
-    struct unary_expr : sor<factor, whitespace, seq<unary_operator, whitespace, unary_operator>> {};
+    struct unary_expr : sor<seq<unary_operator, ws, unary_expr>, factor> {};
 
     struct mul_expr;
     struct div_expr;
@@ -48,11 +48,11 @@ namespace calc {
     struct add_expr;
     struct comp_expr;
 
-    struct mul_op : seq<whitespace, sor<one<'*'>, one<'%'>, one<'~'>>, whitespace, mul_expr> {};
-    struct div_op : seq<whitespace, one<'/'>, whitespace, div_expr> {};
-    struct sub_op : seq<whitespace, one<'-'>, whitespace, sub_expr> {};
-    struct add_op : seq<whitespace, one<'+'>, whitespace, add_expr> {};
-    struct comp_op : seq<whitespace, comp_operator, comp_expr> {};
+    struct mul_op : seq<ws, sor<one<'*'>, one<'%'>, one<'~'>>, ws, mul_expr> {};
+    struct div_op : seq<ws, one<'/'>, ws, div_expr> {};
+    struct sub_op : seq<ws, one<'-'>, ws, sub_expr> {};
+    struct add_op : seq<ws, one<'+'>, ws, add_expr> {};
+    struct comp_op : seq<ws, comp_operator, comp_expr> {};
 
     struct mul_expr : seq<unary_expr, opt<mul_op>> {};
     struct div_expr : seq<mul_expr, opt<div_op>> {};
@@ -60,19 +60,17 @@ namespace calc {
     struct add_expr : seq<sub_expr, opt<add_op>> {};
     struct comp_expr : seq<add_expr, opt<comp_op>> {};
 
-    struct and_expr : seq<comp_expr, opt<seq<whitespace, one<'^'>, whitespace, comp_expr>>> {};
-    struct or_expr : seq<and_expr, opt<seq<whitespace, one<'|'>, whitespace, and_expr>>> {};
-    struct expression : seq<or_expr, opt<seq<whitespace, string<'=','>'>, whitespace, or_expr>>> {};
+    struct and_expr : seq<comp_expr, opt<seq<ws, one<'^'>, ws, comp_expr>>> {};
+    struct or_expr : seq<and_expr, opt<seq<ws, one<'|'>, ws, and_expr>>> {};
+    struct expression : seq<or_expr, opt<seq<ws, string<'=','>'>, ws, or_expr>>> {};
 
-    struct statement : sor<
-        seq<symbol, whitespace, one<'='>, whitespace, expression>,
-        seq<symbol, whitespace, one<':'>, one<'='>, whitespace, expression>,
-        seq<symbol, whitespace, one<':'>, whitespace, expression, whitespace, one<'='>, whitespace, expression>,
-        seq<symbol, whitespace, one<':'>, whitespace, expression>,
-        expression
-    > {};
+    struct set : seq<one<'='>, ws, expression> {};
+    struct infer : seq<string<':', '='>, ws, expression> {};
+    struct declare : seq<one<':'>, ws, expression, opt<set>> {};
 
-    struct grammar : must<statement, whitespace, eof> {};
+    struct statement : seq<expression, opt<ws, sor<infer, set, declare>>> {};
+
+    struct grammar : seq<statement, ws> {};
 }
 
 #endif
--- src/calc.cpp
+++ src/calc.cpp
@@ -7,16 +7,52 @@
 
 #include "calc.hpp"
 #include <data/numbers.hpp>
+#include <map>
 
 namespace Cosmos {
 
     namespace pegtl = tao::pegtl;
 
+    struct value : either<Q, data::string> {
+
+        static value number (const Q &q) {
+            return value {q};
+        }
+
+        static value symbol (const string &x) {
+            return value {x};
+        }
+
+        static value string (const string &) {
+            throw exception {} << "we don't do strings yet ";
+        }
+
+        Q get (const std::map<data::string, value> &Vars) const {
+            if (std::holds_alternative<data::string> (*this)) {
+
+                data::string var = std::get<data::string> (*this);
+                auto val = Vars.find (var);
+
+                if (val == Vars.end ()) throw exception {} << "no variable " << var;
+
+                return val->second.get (Vars);
+            }
+
+            return std::get<Q> (*this);
+        }
+
+    private:
+        either<Q, data::string>::either;
+    };
+
     struct evaluation {
-        stack<Q> Stack;
+        stack<value> Stack;
+        std::map<string, value> &Vars;
+
+        evaluation (std::map<string, value> &v) : Stack {}, Vars {v} {}
 
         void read_symbol (const string &in) {
-            throw exception {} << "read symbol " << in;
+            Stack <<= value::symbol (in);
         }
 
         void read_string (const string &in) {
@@ -24,25 +60,33 @@ namespace Cosmos {
         }
 
         void read_number (const string &in) {
-            Stack <<= Q {Z {in}};
+            Stack <<= value::number (Q {Z {in}});
         }
 
         void mul () {
-            Stack = prepend (rest (rest (Stack)), first (Stack) * first (rest (Stack)));
+            Stack = prepend (rest (rest (Stack)), value::number (first (rest (Stack)).get (Vars) * first (Stack).get (Vars)));
         }
 
         void div () {
-            auto denominator = math::nonzero<Q> {first (Stack)};
+            auto denominator = math::nonzero<Q> {first (Stack).get (Vars)};
             if (!denominator.valid ()) throw exception {} << "division by zero";
-            Stack = prepend (rest (rest (Stack)), first (rest (Stack)) / denominator);
+            Stack = prepend (rest (rest (Stack)), value::number (first (rest (Stack)).get (Vars) / denominator));
         }
 
         void plus () {
-            Stack = prepend (rest (rest (Stack)), first (Stack) + first (rest (Stack)));
+            Stack = prepend (rest (rest (Stack)), value::number (first (rest (Stack)).get (Vars) + first (Stack).get (Vars)));
         }
 
         void minus () {
-            Stack = prepend (rest (rest (Stack)), first (Stack) - first (rest (Stack)));
+            Stack = prepend (rest (rest (Stack)), value::number (first (rest (Stack)).get (Vars) - first (Stack).get (Vars)));
+        }
+
+        void set () {
+            data::string var = std::get<data::string> (first (rest (Stack)));
+            if (Vars.find (var) != Vars.end ()) throw exception {} << "variable " << var << " is already defined!";
+            auto val = first (Stack);
+            Vars.insert(std::pair {var, val});
+            Stack = stack<value> {val};
         }
     };
 
@@ -97,27 +141,48 @@ namespace Cosmos {
         }
     };
 
-    std::ostream &write (std::ostream &o, const Q &q) {
+    template <> struct eval_action<calc::expression> {
+        template <typename Input>
+        static void apply (const Input& in, evaluation &eval) {}
+    };
+
+    template <> struct eval_action<calc::set> {
+        template <typename Input>
+        static void apply (const Input& in, evaluation &eval) {
+            eval.set ();
+        }
+    };
+
+    std::ostream &write (std::ostream &o, const value &v) {
+        Q q = std::get<Q> (v);
         o << q.Numerator;
         if (q.Denominator != 1) o << "/" << q.Denominator;
         return o;
     }
 
+    template <> struct eval_action<calc::statement> {
+        template <typename Input>
+        static void apply (const Input& in, evaluation &eval) {
+            if (!data::empty (eval.Stack)) write (std::cout << "\n result: ", eval.Stack.first ()) << std::endl;
+        }
+    };
+
     void calc () {
         std::string input_str;
         std::cout << "calculator app engaged." << std::endl;
+
+        std::map<string, value> vars;
+
         while (true) {
-            std::cout << "  input: ";
+            std::cout << "\n input: ";
 
             if (!std::getline (std::cin, input_str)) break;
             if (input_str.empty ()) continue;
 
             try {
                 tao::pegtl::memory_input<> input (input_str, "expression");
-                evaluation eval {};
+                evaluation eval {vars};
                 tao::pegtl::parse<calc::grammar, eval_action> (input, eval);
-
-                write (std::cout << " result: ", eval.Stack.first ()) << std::endl;
 
             } catch (const std::exception& ex) {
                 std::cerr << "Error: " << ex.what () << std::endl;
